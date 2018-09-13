2cc7b2ac91d20c181477402766fbb7813d261bcd
V S Bharath <bharathcse5t6@gmail.com>
2018-09-14 01:01:46 +0530

    learned about maps,refactored my code using es6

--- src/index.js
+++ src/index.js
@@ -1,17 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import Card from './Card';
+//import Card from './Card';
+import CardList from './CardList'
 import registerServiceWorker from './registerServiceWorker';
 import 'tachyons' 
 import {kids} from './kids'
 // I want react dom to use a method called render
 ReactDOM.render(
-<div>
-    <Card id={kids[0].id} name={kids[0].name} email={kids[0].email}/>
-    <Card id={kids[1].id} name={kids[1].name} email={kids[1].email}/>
-    <Card id={kids[2].id} name={kids[2].name} email={kids[2].email}/>
-    
-</div>, 
+<CardList kids={kids}/>, 
 document.getElementById('root'));
 registerServiceWorker();
