01cb6e2a24960c25ccdab629fc8944959837443e
V S Bharath <bharathcse5t6@gmail.com>
2018-09-14 00:35:44 +0530

    Added cards,used es6 destructuring,required tachyons

--- src/index.js
+++ src/index.js
@@ -1,8 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import Card from './Card';
 import registerServiceWorker from './registerServiceWorker';
-
-ReactDOM.render(<App />, document.getElementById('root'));
+import 'tachyons' 
+import {kids} from './kids'
+// I want react dom to use a method called render
+ReactDOM.render(
+<div>
+    <Card id={kids[0].id} name={kids[0].name} email={kids[0].email}/>
+    <Card id={kids[1].id} name={kids[1].name} email={kids[1].email}/>
+    <Card id={kids[2].id} name={kids[2].name} email={kids[2].email}/>
+    
+</div>, 
+document.getElementById('root'));
 registerServiceWorker();
